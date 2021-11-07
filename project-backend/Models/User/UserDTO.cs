267e1680d31e27d734c715abaf36f6d1aaa3fd32
Barzu Andrei-Daniel <andrewbarzu@gmail.com>
2021-11-07 17:43:03 +0200

    Set up test project

---
+++ project-backend/Models/User/UserDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace project_backend.Models.User
+{
+    public class UserDTO
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+    }
+}
