e05de11262d76f4eef44fc33ba17cf12c188ad69
Andrei-Daniel Barzu <37110918+AndrewBarzu@users.noreply.github.com>
2021-11-08 17:36:24 +0200

    Update UserDAO.cs
    Fixed typo

--- project-backend/Models/DAOs/UserDAO.cs
+++ project-backend/Models/DAOs/UserDAO.cs
@@ -9,7 +9,7 @@ namespace project_backend.Models.User
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string FirstName { get; set; }
-        public string LastNAme { get; set; }
+        public string LastName { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
