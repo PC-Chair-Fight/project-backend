9bdf23967d4bc518ac157d157f3942ac417c833f
Cata <catab212@gmail.com>
2021-11-10 14:35:37 +0200

    Improved readability of the code in the JobController

--- project-backend/Controllers/JobController.cs
+++ project-backend/Controllers/JobController.cs
@@ -6,6 +6,7 @@ using project_backend.Models;
 using project_backend.Models.Job;
 using project_backend.Models.JobController.GetJobs;
 using project_backend.Providers.JobProvider;
+using project_backend.Utils;
 using System;
 using System.Linq;
 
@@ -38,115 +39,27 @@ namespace project_backend.Controllers
                 return Unauthorized(new Error("Not logged in"));
             }
 
-            var jobs = _jobProvider.QueryJobs();
-            if (query.ByCurrentUserOnly ?? false)
-            {
-                jobs = jobs.Where(j => j.UserId == userId);
-            }
-
-            if (query.OlderThan != null)
-            {
-                jobs = jobs.Where(j => j.PostDate <= query.OlderThan);
-            }
-
-            if (query.NewerThan != null)
-            {
-                jobs = jobs.Where(j => j.PostDate >= query.NewerThan);
-            }
-
-            if (query.FilterFields != null)
-            {
-                for (int i = 0; i < query.FilterFields.Length; ++i)
+            var returnValue = _jobProvider
+                .QueryJobs()
+                .If(query.ByCurrentUserOnly ?? false,
+                    q => q.Where(j => j.UserId == userId))
+                .If(query.OlderThan != null,
+                    q => q.Where(j => j.PostDate <= query.OlderThan))
+                .If(query.NewerThan != null,
+                    q => q.Where(j => j.PostDate >= query.NewerThan))
+                .Filter(query.FilterFields, query.FilterValues, query.ExactFilters)
+                .Sort(query.OrderBy, query.Ascending)
+                .Skip(query.Index)
+                .Take(query.Count)
+                .Select(j => new GetJobsResponseObject.GetJobsResponseEntry
                 {
-                    var field = query.FilterFields[i];
-                    var value = query.FilterValues[i];
-                    var exact = query.ExactFilters[i];
-
-                    jobs = field switch
-                    {
-                        // look for id, ignore exact
-                        GetJobsQueryObject.FilterField.Id => jobs.Where(j => j.Id == int.Parse(value)),
-
-                        GetJobsQueryObject.FilterField.Name => exact ?
-                                jobs.Where(j => j.Name == value) :
-                                jobs.Where(j => j.Name.Contains(value)),
-
-                        GetJobsQueryObject.FilterField.Description => exact ?
-                                jobs.Where(j => j.Description == value) :
-                                jobs.Where(j => j.Description.Contains(value)),
-
-                        // filter by Done, ignore exact
-                        GetJobsQueryObject.FilterField.Done => jobs = jobs.Where(j => j.Done == bool.Parse(value)),
-
-                        _ => jobs
-                    };
-                }
-            }
-
-            if (query.OrderBy != null && query.OrderBy.Length >= 1)
-            {
-                var orderChain = query.OrderBy[0] switch
-                {
-                    GetJobsQueryObject.OrderField.Id => query.Ascending[0] ?
-                            jobs.OrderBy(j => j.Id) :
-                            jobs.OrderByDescending(j => j.Id),
-
-                    GetJobsQueryObject.OrderField.Name => query.Ascending[0] ?
-                            jobs.OrderBy(j => j.Name) :
-                            jobs.OrderByDescending(j => j.Name),
-
-                    GetJobsQueryObject.OrderField.Description => query.Ascending[0] ?
-                            jobs.OrderBy(j => j.Description) :
-                            jobs.OrderByDescending(j => j.Description),
-
-                    GetJobsQueryObject.OrderField.PostDate => query.Ascending[0] ?
-                            jobs.OrderBy(j => j.PostDate) :
-                            jobs.OrderByDescending(j => j.PostDate),
-
-                    _ => jobs.OrderBy(j => j.Id)
-                };
-                for (var i = 0; i < query.OrderBy.Length; i++)
-                {
-                    orderChain = query.OrderBy[i] switch
-                    {
-                        GetJobsQueryObject.OrderField.Id => query.Ascending[i] ?
-                                orderChain.ThenBy(j => j.Id) :
-                                orderChain.ThenByDescending(j => j.Id),
-
-                        GetJobsQueryObject.OrderField.Name => query.Ascending[i] ?
-                                orderChain.ThenBy(j => j.Name) :
-                                orderChain.ThenByDescending(j => j.Name),
-
-                        GetJobsQueryObject.OrderField.Description => query.Ascending[i] ?
-                                orderChain.ThenBy(j => j.Description) :
-                                orderChain.ThenByDescending(j => j.Description),
-
-                        GetJobsQueryObject.OrderField.PostDate => query.Ascending[i] ?
-                                orderChain.ThenBy(j => j.PostDate) :
-                                orderChain.ThenByDescending(j => j.PostDate),
-                        
-                        _ => jobs.OrderBy(j => j.Id)
-                    };
-                }
-
-                jobs = orderChain;
-            }
-            else
-            {
-                jobs = jobs.OrderBy(j => j.Id);
-            }
-
-            jobs = jobs.Skip(query.Index).Take(query.Count);
-
-            var returnValue = jobs.Select(j => new GetJobsResponseObject.GetJobsResponseEntry
-            {
-                Id = j.Id,
-                Name = j.Name,
-                Description = j.Description,
-                Done = j.Done,
-                PostDate = j.PostDate,
-                UserId = j.UserId
-            }).ToArray();
+                    Id = j.Id,
+                    Name = j.Name,
+                    Description = j.Description,
+                    Done = j.Done,
+                    PostDate = j.PostDate,
+                    UserId = j.UserId
+                }).ToArray();
 
             _logger.LogInformation("GetJobs -> {0}", returnValue);
 
@@ -154,6 +67,112 @@ namespace project_backend.Controllers
             {
                 jobs = returnValue
             });
+        }
+    }
+
+    static class JobQueryExtensions
+    {
+        public static IQueryable<JobDAO> Filter(
+            this IQueryable<JobDAO> jobs,
+            GetJobsQueryObject.FilterField[] filterFields,
+            string[] filterValues,
+            bool[] exactFilters
+        )
+        {
+            if (filterFields == null || filterValues == null || exactFilters == null)
+                return jobs;
+            for (int i = 0; i < filterFields.Length; ++i)
+            {
+                var field = filterFields[i];
+                var value = filterValues[i];
+                var exact = exactFilters[i];
+
+                jobs = field switch
+                {
+                    // look for id, ignore exact
+                    GetJobsQueryObject.FilterField.Id => jobs.Where(j => j.Id == int.Parse(value)),
+
+                    GetJobsQueryObject.FilterField.Name => exact ?
+                            jobs.Where(j => j.Name == value) :
+                            jobs.Where(j => j.Name.Contains(value)),
+
+                    GetJobsQueryObject.FilterField.Description => exact ?
+                            jobs.Where(j => j.Description == value) :
+                            jobs.Where(j => j.Description.Contains(value)),
+
+                    // filter by Done, ignore exact
+                    GetJobsQueryObject.FilterField.Done => jobs = jobs.Where(j => j.Done == bool.Parse(value)),
+
+                    _ => jobs
+                };
+            }
+            return jobs;
+        }
+
+        public static IOrderedQueryable<JobDAO> BeginSort(
+            this IQueryable<JobDAO> jobs,
+            GetJobsQueryObject.OrderField orderBy,
+            bool ascending
+        )
+        {
+            return orderBy switch
+            {
+                GetJobsQueryObject.OrderField.Id => ascending ?
+                        jobs.OrderBy(j => j.Id) :
+                        jobs.OrderByDescending(j => j.Id),
+
+                GetJobsQueryObject.OrderField.Name => ascending ?
+                        jobs.OrderBy(j => j.Name) :
+                        jobs.OrderByDescending(j => j.Name),
+
+                GetJobsQueryObject.OrderField.Description => ascending ?
+                        jobs.OrderBy(j => j.Description) :
+                        jobs.OrderByDescending(j => j.Description),
+
+                GetJobsQueryObject.OrderField.PostDate => ascending ?
+                        jobs.OrderBy(j => j.PostDate) :
+                        jobs.OrderByDescending(j => j.PostDate),
+
+                _ => jobs.OrderBy(j => j.Id)
+            };
+        }
+
+        public static IQueryable<JobDAO> Sort(
+            this IQueryable<JobDAO> jobs,
+            GetJobsQueryObject.OrderField[] orderBy,
+            bool[] ascending
+        )
+        {
+            if (orderBy == null || orderBy.Length < 1)
+            {
+                return jobs.OrderBy(j => j.Id);
+            }
+            var orderChain = jobs.BeginSort(orderBy[0], ascending[0]);
+            for (var i = 1; i < orderBy.Length; i++)
+            {
+                orderChain = orderBy[i] switch
+                {
+                    GetJobsQueryObject.OrderField.Id => ascending[i] ?
+                            orderChain.ThenBy(j => j.Id) :
+                            orderChain.ThenByDescending(j => j.Id),
+
+                    GetJobsQueryObject.OrderField.Name => ascending[i] ?
+                            orderChain.ThenBy(j => j.Name) :
+                            orderChain.ThenByDescending(j => j.Name),
+
+                    GetJobsQueryObject.OrderField.Description => ascending[i] ?
+                            orderChain.ThenBy(j => j.Description) :
+                            orderChain.ThenByDescending(j => j.Description),
+
+                    GetJobsQueryObject.OrderField.PostDate => ascending[i] ?
+                            orderChain.ThenBy(j => j.PostDate) :
+                            orderChain.ThenByDescending(j => j.PostDate),
+
+                    _ => jobs.OrderBy(j => j.Id)
+                };
+            }
+
+            return orderChain;
         }
     }
 }
---
+++ project-backend/Utils/LinqExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace project_backend.Utils
+{
+    public static class LinqExtensions
+    {
+        // allows using conditions within a linq query
+        public static IQueryable<TSource> If<TSource>(
+            this IQueryable<TSource> source,
+            bool condition,
+            Func<IQueryable<TSource>, IQueryable<TSource>> @then,
+            Func<IQueryable<TSource>, IQueryable<TSource>> @else = null
+        ) => condition ? 
+            @then(source) : 
+            @else != null ? 
+                @else(source) : 
+                source;
+
+        // allows using conditions within a linq query
+        public static IEnumerable<TSource> If<TSource>(
+            this IEnumerable<TSource> source,
+            bool condition,
+            Func<IEnumerable<TSource>, IEnumerable<TSource>> @then,
+            Func<IEnumerable<TSource>, IEnumerable<TSource>> @else = null
+        ) => condition ?
+            @then(source) :
+            @else != null ?
+                @else(source) :
+                source;
+    }
+}
