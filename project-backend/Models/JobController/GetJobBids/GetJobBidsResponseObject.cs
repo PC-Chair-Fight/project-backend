eaa40115887aa4f2d196102cfd7b929ae00bc4ba
AndreiPetrusMihai <andreipetrus2000@gmail.com>
2021-12-01 14:04:18 +0200

    feat(get-job-bids): added an endpoint for getting the bids of a job

---
+++ project-backend/Models/JobController/GetJobBids/GetJobBidsResponseObject.cs
@@ -0,0 +1,30 @@
+﻿using project_backend.Models.Bid;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace project_backend.Models.JobController.GetJobBids
+{
+    public class GetJobBidsResponseObject
+    {
+        public List<JobBidDTO> Bids { get; set; }
+        public GetJobBidsResponseObject(IQueryable<BidDAO> bidsInput)
+        {
+            Bids = bidsInput.Select(b => new JobBidDTO
+            {
+                Id = b.Id,
+                JobId = b.JobId,
+                Sum = b.Sum,
+                WorkerId = b.WorkerId
+            }
+            ).ToList();
+        }
+    }
+
+    public class JobBidDTO
+    {
+        public int Id { get; set; }
+        public float Sum { get; set; }
+        public int WorkerId { get; set; }
+        public int JobId { get; set; }
+    }
+}
